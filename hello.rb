f89d1f931b1b74c9bc68460a27dc1a4661c0525d
Lorenzo Turrino <neuronefunzionante@gmail.com>
2016-01-30 13:19:31 +0000

    fix hello.rb bug

--- hello.rb
+++ hello.rb
@@ -2,7 +2,7 @@ puts "hello world! this is a ruby script!"
 
 puts (1..10).to_a
 
-10.downto(0) { |i| print i + " "}
+10.downto(0) { |i| print i.to_s + " "}
 puts "go!"
 
-puts "\n this should be created on a branch diff from master and readme-edit"
+puts "\n     this should be created on a branch diff from master and readme-edit"
